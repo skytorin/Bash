2785274e1e06b5bea397567b4accb1680096f522
SkyTorin <nick555@bk.ru>
2022-01-27 17:36:27 +0300

    Edited 27.01.2022 17:36:00

--- git_pull_all_projects.sh
+++ git_pull_all_projects.sh
@@ -3,9 +3,10 @@
 cd $HOME/My_Projects
 for i in $(ls); 
     do 
-        echo -e "\033[32m Git Pull $i:"; 
+        echo -e "\033[32m -= Git Pull $i =- \033[0m"
 	cd $HOME/My_Projects/$i 
 	git pull
+	echo -e "\033[0m" 
     done;
 
 
